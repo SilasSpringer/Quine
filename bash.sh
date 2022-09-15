a8213bfe1b914efe6bb3d7c8e66a2dbf5c7b2540
SilasSpringer <ss132517@ohio.edu>
2022-09-14 20:22:55 -0400

    bash.sh

--- bash.sh
+++ bash.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "================="
-echo "source code for bash file"
-cat bash.sh
-echo "================="
-echo "source code for python file"
+echo "source code for bash file {"
+grep "^.*$" bash.sh
+echo -e "}\n================="
+echo "source code for python file {"
 python python.py
 
 g++ -Wall -Werror -g -o cpp cpp.cc 
 
-echo "source code for C++ file"
+echo -e "}\nsource code for C++ file {"
 ./cpp
 
-echo "this has been a quine written in about 30 minutes"
+echo -e "}\nthis has been a quine written in about 30 minutes"
 echo -e "woo ACM ftw\n\n\n"
 echo "so long"
 echo "and thanks for the pizza"
